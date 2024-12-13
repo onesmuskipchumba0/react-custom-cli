5975aa51c4bc77d9bacce61b38a130eacc26d909
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-12-13 18:58:19 +0300

    add vite-tailwind-template

---
+++ cli/templates/vite-tailwind-template/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container flex flex-col items-center justify-center flex-1 h-[100vh]">
+        <h4 className="text-xl">Hello world from Vite</h4>
+        <h4 className="text-primary">Edit App.jsx to start working</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
