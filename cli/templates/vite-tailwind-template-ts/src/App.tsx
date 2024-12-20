f747508c83172c89591a3064c36cfa6e5bdd4aff
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-12-20 15:46:21 +0300

    add vite ts template

---
+++ cli/templates/vite-tailwind-template-ts/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container flex flex-1 flex-col justify-center items-center h-[100vh]">
+        <h1 className="text-2xl">🥳 Welcome to Custom JS - Vite Typescript Template 🚀</h1>
+        <h2 className="text-lg text-blue-300">Edit App.tsx to see changes</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
